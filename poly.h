a07c6c02c4838cdd98b5240bed42ebcc2729e286
Laurent Claessens <moky.math@gmail.com>
2015-11-16 11:21:19 +0100

    Work in progress on polyno::iterator

--- poly.h
+++ poly.h
@@ -1,14 +1,23 @@
+#ifndef  __POLY_H__
+#define  __POLY_H__
+
+
 class polynom
 {
     private :
         smartp   first;
 
     public :
-    class iterator
-    {
-        private : 
-            smartp  mono_ptr;
-        public 
-            polynom::iterator& operator++(int);
-    }
+        polynom(int);
+        class iterator
+        {
+            private : 
+                smartp  mono_ptr;
+            public 
+                iterator& operator++(int);
+        }
+        iterator
+
 }
+
+#endif      // __POLY_H__
