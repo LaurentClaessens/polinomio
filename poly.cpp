17b3d1367b88247de55f80b57b0484a864f44b62
Andrey Petrov <apetrov.ya@yandex.ru>
2015-11-16 11:33:15 +0100

    Aggiornati i miei fiel

--- poly.cpp
+++ poly.cpp
@@ -1,5 +1,16 @@
 
-polynom::iterator& operator++(int)
+polynom::iterator& operator++()  // ++itr 
 {
- if (mono_ptr) mono_ptr=mono_ptr.next;
+    if (mono_ptr) mono_ptr=mono_ptr->next;
+    return *this;
 }
+
+
+polynom::iterator operator++(int)   //itr++
+{
+}
+
+polynom::polynom(int c)
+{
+
+}<++>
